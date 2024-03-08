c8e1a58065e1cdf151c74a7d7ff31f3e92f8e66a
Aobies59 <alvaroobiesgarcia@proton.me>
2024-03-08 14:40:51 +0100

    Improved Grammar+

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ def main(argv):
         print("Usage: python main.py <filenames>")
         sys.exit(1)
 
-    for filename in argv[1:]:
-        if filename == "":
-            continue
+    elif len(argv) == 2:
         with open(filename, 'r') as file:
             code = file.read()
-            parser = ParserClass()
+            parser = ParserClass(filename)
             parser.test(code)
-            print("==========================================================")
+            sys.exit(0)
+
+    print("==========================================================")
+    for filename in argv[1:]:
+        if filename == "":
+            continue  # skip empty filename (in case script is being called with run.sh)
+        with open(filename, 'r') as file:
+            code = file.read()
+            parser = ParserClass(filename)
+            parser.test(code)
+        print("==========================================================")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main(sys.argv)
--- parser.py
+++ parser.py
@@ -3,30 +3,35 @@ from lexer import LexerClass
 
 
 class ParserClass:
-    def __init__(self):
+    def __init__(self, filename):
         self.lexer = LexerClass()
         self.tokens = LexerClass.tokens
         self.parser = yacc.yacc(module=self)
+        self.filename = filename
 
     def p_axiom(self, p):
         """axiom : OPEN_DELIMITER element CLOSE_DELIMITER
         | empty"""
         if len(p) == 4:
             p[0] = p[2]
-            for i in p[0]:
-                print("{" + i + "}")
+            print(f'FICHERO AJSON "{self.filename}"')
+            print(p[0])
         else:
             p[0] = []
-            print("Empty file")
+            print(f'FICHERO AJSON VACIO "{self.filename}"')
 
     def p_element(self, p):
-        """element : stringvalue COMMA element
-        | stringvalue possiblecomma
+        """element : stringvalue nextelement
         | empty"""
-        if len(p) == 4:
-            p[0] = [p[1]] + p[3]
+        p[0] = p[1]
+
+    def p_nextelement(self, p):
+        """nextelement : possiblecomma
+        | COMMA element"""
+        if len(p) == 3:
+            p[0] = p[2]
         else:
-            p[0] = [p[1]]
+            p[0] = ""
 
     def p_possiblecomma(self, p):
         """possiblecomma : COMMA
@@ -35,7 +40,11 @@ class ParserClass:
 
     def p_stringvalue(self, p):
         """stringvalue : string COLON value"""
-        p[0] = f"{p[1]}: {p[3]}".strip("'")
+        # check if value starts with a dot
+        if isinstance(p[3], str) and p[3].startswith('.'):
+            p[0] = f"{p[1]}{p[3]}"
+        else:
+            p[0] = f"{p[1]}: {p[3]}\n".strip("'")
     
     def p_string(self, p):
         """string : QUOTED_STRING
@@ -53,7 +62,7 @@ class ParserClass:
 
     def p_nested_element(self, p):
         'nested_element : OPEN_DELIMITER element CLOSE_DELIMITER'
-        p[0] = p[2]
+        p[0] = f".{p[2]}"
 
     def p_number(self, p):
         """number : INT
@@ -61,7 +70,7 @@ class ParserClass:
         | BINARY
         | OCTAL
         | HEXADECIMAL"""
-        p[0] = p[1]
+        p[0] = p[1]  # number can't be string, as it will be used to operate
 
     def p_operation(self, p):
         """operation : equal_operation
@@ -69,31 +78,31 @@ class ParserClass:
         | greater_or_equal_operation
         | less_operation
         | less_or_equal_operation"""
-        p[0] = p[1]
+        p[0] = p[1]  # all operations are already strings
 
     def p_equal_operation(self, p):
         'equal_operation : number EQUAL number'
-        p[0] = p[1] == p[3]
+        p[0] = str(p[1] == p[3])
         
 
     def p_greater_operation(self, p):
         'greater_operation : number GREATER number'
-        p[0] = p[1] > p[3]
+        p[0] = str(p[1] > p[3])
         
     
     def p_greater_or_equal_operation(self, p):
         'greater_or_equal_operation : number GREATER_OR_EQUAL number'
-        p[0] = p[1] >= p[3]
+        p[0] = str(p[1] >= p[3])
         
 
     def p_less_operation(self, p):
         'less_operation : number LESS number'
-        p[0] = p[1] < p[3]
+        p[0] = str(p[1] < p[3])
         
 
     def p_less_or_equal_operation(self, p):
         'less_or_equal_operation : number LESS_OR_EQUAL number'
-        p[0] = p[1] <= p[3]
+        p[0] = str(p[1] <= p[3])
         
 
     def p_error(self, p):
